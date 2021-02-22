78f52f2bcc06f16a9e1dba78d4b7ad2759298952
BETSSON\maga09 <marco.galea@betssongroup.com>
2021-02-22 16:34:33 +0100

    Added a new comment

--- TestApp/Startup.cs
+++ TestApp/Startup.cs
@@ -26,6 +26,7 @@ namespace TestApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //This is a comment
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "TestApp", Version = "v1"}); });
         }
